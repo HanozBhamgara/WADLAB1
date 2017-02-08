116de8547724f39f76812ebeca0ec0a0e265f042
Hanoz Bhamgara <2191455b@student.gla.ac.uk>
2017-02-08 23:12:49 +0000

    ch9 before 9.7

--- rango/views.py
+++ rango/views.py
@@ -4,27 +4,27 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
+
 
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
-    # construct a dictionary to pass to the template engine as its context
-    # Note the key boldmessage is the same as {{boldmessage}} in the template
 
     page_list = Page.objects.order_by('views')[:5]
     context_dict = {'categories': category_list, 'pages':page_list}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+
     return render(request, 'rango/index.html', context_dict)
 
     #return HttpResponse("Rango says hey there partner! <Bbr/> <a> href='/rango/about/'>About</a>")
 
 def about(request):
 
-    return render(request, 'rango/about.html')
-    #return HttpResponse("This tutorial has been put together by <Hanoz> <Bbr/> Rango says here is the about page <a href='/rango/'>Index</a>")
-
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -68,7 +68,8 @@ def add_category(request):
         # Have we been provided with a valid form?
         if form.is_valid():
             # Save the new category to the database.
-            form.save(commit=True)
+            category = form.save(commit=True)
+            print(category,category.slug)
             # Now that the category is saved
             # We could give a confirmation message
             # But since the most recent category added is on the index page
@@ -107,3 +108,63 @@ def add_page(request, category_name_slug):
     context_dict = {'form': form, 'category': category}
 
     return render(request, 'rango/add_page.html', context_dict)
+
+def register(request):
+    # A boolean value for telling the template
+    # whether the registration was successful.
+    # Set to False initially. Code changes value to
+    # True when registration succeeds.
+    registered = False
+
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+        # Attempt to grab information from the raw form information.
+        # Note that we make use of both UserForm and UserProfileForm.
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        # If the two forms are valid...
+        if user_form.is_valid() and profile_form.is_valid():
+            # Save the user's form data to the database.
+            user = user_form.save()
+
+            # Now we hash the password with the set_password method.
+            # Once hashed, we can update the user object.
+            user.set_password(user.password)
+            user.save()
+            # Now sort out the UserProfile instance.
+            # Since we need to set the user attribute ourselves,
+            # we set commit=False. This delays saving the model
+            # until we're ready to avoid integrity problems.
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            # Did the user provide a profile picture?
+            # If so, we need to get it from the input form and
+            #put it in the UserProfile model.
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            # Now we save the UserProfile model instance.
+            profile.save()
+
+            # Update our variable to indicate that the template
+            # registration was successful.
+            registered = True
+
+        else:
+            # Invalid form or forms - mistakes or something else?
+            # Print problems to the terminal.
+             print(user_form.errors, profile_form.errors)
+    else:
+        # Not a HTTP POST, so we render our form using two ModelForm instances.
+        # These forms will be blank, ready for user input.
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    # Render the template depending on the context.
+    return render(request,
+            'rango/register.html',
+            {'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': registered})
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'Options': {'min_length': 6}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 
 # Internationalization
